6baef3244f4347b361da9a646c6650d7f629f5f4
Benjamin Carlisle <bjamin.carlisle@gmail.com>
2017-01-18 09:29:42 -0700

    Distributor reads in only wanted fields and then sticks them into a Post struct. They are then all put into an array of Posts

--- distributor.go
+++ distributor.go
@@ -1,4 +1,57 @@
 package main
 
+import (
+	"io/ioutil"
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
+type Post struct {
+	Url    	  	string    `json:"activity_url"`
+	Text   	  	string    `json:"cleaned_body_text"`
+	RawText   	string    `json:"raw_body_text"`
+	Country	  	string    `json:"country"`
+	CountryCode	string    `json:"country_code"`
+	Region		string    `json:"region"`
+	RegionCode	string    `json:"region_code"`
+	Language	string    `json:"language"`
+	Latitude	float64   `json:"latitude"`
+	Longitude	float64   `json:"longitude"`
+}
+
+func (p Post) toString() string {
+	return toJson(p)
+}
+
+func toJson(p interface{}) string {
+	bytes, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return string(bytes)
+}
+
 func main() {
+
+	posts := getPosts()
+	for _, p := range posts {
+		fmt.Println(p.toString())
+	}
+
+	fmt.Println(toJson(posts))
+}
+
+func getPosts() []Post {
+	raw, err := ioutil.ReadFile("data/normalized-payloads.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var c []Post
+	json.Unmarshal(raw, &c)
+	return c
 }
