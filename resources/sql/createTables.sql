1f23124452684206e86f4bebe2eba87704a5a811
jvansteeter <spam.magnet1991@gmail.com>
2017-04-11 13:13:26 -0600

    Added the complete code to add processed data to the database, however I think there is currently an issue with the trie

--- resources/sql/createTables.sql
+++ resources/sql/createTables.sql
@@ -1,6 +1,6 @@
 create table post
 (
-  UUID varchar(128) DEFAULT uuid_generate_v1() PRIMARY KEY,
+  UUID varchar(128) NOT NULL PRIMARY KEY,
   COLLECTED timestamp DEFAULT now(),
   threat boolean,
   POST bytea
@@ -8,6 +8,7 @@ create table post
 
 create table processed_post
 (
+  UUID varchar(128) REFERENCES post(UUID) ON DELETE CASCADE NOT NULL,
   post_length int,
   like_count int,
   followers_count int,
@@ -16,5 +17,21 @@ create table processed_post
   retweet_count int,
   is_retweet BOOLEAN,
   klout_score int,
-  exclaimation_count int
+  exclaimation_count int,
+  active_shooter BOOLEAN DEFAULT FALSE,
+  attack BOOLEAN DEFAULT FALSE,
+  attacker BOOLEAN DEFAULT FALSE,
+  bomb BOOLEAN DEFAULT FALSE,
+  bomb_threat BOOLEAN DEFAULT FALSE,
+  breaking BOOLEAN DEFAULT FALSE,
+  danger BOOLEAN DEFAULT FALSE,
+  dead BOOLEAN DEFAULT FALSE,
+  gunman BOOLEAN DEFAULT FALSE,
+  killing BOOLEAN DEFAULT FALSE,
+  rape BOOLEAN DEFAULT FALSE,
+  shooter BOOLEAN DEFAULT FALSE,
+  shooting BOOLEAN DEFAULT FALSE,
+  stabbing BOOLEAN DEFAULT FALSE,
+  terrorist BOOLEAN DEFAULT FALSE,
+  warning BOOLEAN DEFAULT FALSE
 );
